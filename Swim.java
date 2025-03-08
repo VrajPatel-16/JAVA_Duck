af9fd86788572b42456353fe158d9b0f39a7e1ac
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:35:09 +0530

    Implementing SwimBehavior for ducks that can swim

---
+++ Swim.java
@@ -0,0 +1,9 @@
+// Swim.java 
+
+//Implements swimming behavior
+public class Swim implements SwimBehavior {
+    @Override
+    public void swim() {
+        System.out.println("I can swim in the water!");
+    }
+}
