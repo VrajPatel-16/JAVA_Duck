37b7186530c4fe762ed21c160ff0fd4f6eb1fb57
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:41:37 +0530

    implementing QuackBehavior for squeaky ducks

---
+++ Squeak.java
@@ -0,0 +1,9 @@
+// Squeak.java 
+
+//Implements squeaky quack behavior
+public class Squeak implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Squeak! Squeak!");
+    }
+}
