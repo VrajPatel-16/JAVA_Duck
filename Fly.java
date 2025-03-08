e59e0861904496605c225030b59258b753982f1e
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:31:53 +0530

    Implementing FlyBehavior for ducks that can fly

---
+++ Fly.java
@@ -0,0 +1,9 @@
+// Fly.java 
+
+//Implements flying behavior
+public class Fly implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I can fly high in the sky!");
+    }
+}
