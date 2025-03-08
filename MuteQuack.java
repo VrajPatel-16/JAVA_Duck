ae6375c8fa14e960eef514e7b91132c323b1102c
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:40:11 +0530

    implementing QuackBehavior for silent ducks

---
+++ MuteQuack.java
@@ -0,0 +1,9 @@
+// MuteQuack.java 
+
+//Implements mute behavior
+public class MuteQuack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("...");
+    }
+}
