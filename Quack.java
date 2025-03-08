dd0ac7445f3544597ed90b224c0d7c61627c71bd
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:39:13 +0530

    implementing QuackBehavior for normal quacking

---
+++ Quack.java
@@ -0,0 +1,9 @@
+// Quack.java 
+
+//Implements normal quacking behavior
+public class Quack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Quack! Quack!");
+    }
+}
