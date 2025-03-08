51db0562a74d8e557a7316007ece52efb46001bf
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:36:01 +0530

    Implementing SwimBehavior for ducks that float

---
+++ Float.java
@@ -0,0 +1,9 @@
+// Float.java 
+
+// Implements floating behavior
+public class Float implements SwimBehavior {
+    @Override
+    public void swim() {
+        System.out.println("I float on the water.");
+    }
+}
