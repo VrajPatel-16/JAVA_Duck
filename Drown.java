175750c3d3b14fa8d83c1cacda66d2f8f111b407
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-08 19:37:29 +0530

    implementing SwimBehavior for ducks that drown

---
+++ Drown.java
@@ -0,0 +1,9 @@
+// Drown.java 
+
+// Implements drowning behavior
+public class Drown implements SwimBehavior {
+    @Override
+    public void swim() {
+        System.out.println("I sink in the water...");
+    }
+}
